82b0cdaeba8c22d9a5721a1e0429625633abf6f1
Brandon Lightbody <brandon.lightbody14@gmail.com>
2025-06-27 15:51:31 -0500

    Added tailwind.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
   integrations: [tailwind()],
   outDir: 'docs',
-  base: '/Brandon-Lightbody-Pf', // Update this to '/your-repo-name/' if deploying to GitHub Pages
+
+  // Update this to '/your-repo-name/' if deploying to GitHub Pages
+  base: '/Brandon-Lightbody-Pf',
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
