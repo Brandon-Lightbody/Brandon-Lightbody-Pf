7829213a613b5632f0c3468f16d9cc1b2c579f19
Brandon Lightbody <brandon.lightbody14@gmail.com>
2025-06-27 19:30:33 -0500

    testing docs extension to base path.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  base: '/Brandon-Lightbody-Pf/',
+  base: '/Brandon-Lightbody-Pf/Docs/',
   outDir: 'docs',
   integrations: [tailwind()],
 });
