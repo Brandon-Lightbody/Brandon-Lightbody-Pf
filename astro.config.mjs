adf04bc897cb0ad830a6fff5853f93fd6281809c
Brandon Lightbody <brandon.lightbody14@gmail.com>
2025-06-27 19:15:46 -0500

    Reordered defineConfig

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   base: '/Brandon-Lightbody-Pf',
-  integrations: [tailwind()],
   outDir: 'docs',
+  integrations: [tailwind()],
 });
